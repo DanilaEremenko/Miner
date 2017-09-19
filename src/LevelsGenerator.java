e6c092d1efe7b273de06bfba22cd53add2be0043
DanilaEremenko <danilaeremenko@mail.ru>
2017-09-19 23:43:22 +0300

    Update LevelsGenerator.java

--- src/LevelsGenerator.java
+++ src/LevelsGenerator.java
@@ -1,9 +1,7 @@
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Rectangle;
+
 
 
 import java.util.ArrayList;
@@ -26,7 +24,7 @@ public class LevelsGenerator {
 
     public LevelsGenerator(int weight, int hight, int minesDigit) {
         this.weight = weight;
-        this.minesDigit=minesDigit;
+        this.minesDigit = minesDigit;
         this.hight = hight;
         cells = new ArrayList<>();
         bombs = new ArrayList<>();
@@ -41,13 +39,13 @@ public class LevelsGenerator {
         }
         //Все что выше для генерации номеров мин
 
-        for (int i = 1; i <= weight; i++)
-            for (int j = 1; j <= hight; j++) {
-                if (contains(weight * (i - 1) + j, numbersOfMines)) {
-                    bombs.add(new Cell(9, j, i, weight));
-                    cells.add(bombs.get(bombs.size()-1));
+        for (int i = 0; i < weight; i++)
+            for (int j = 0; j < hight; j++) {
+                if (contains(weight * i + j, numbersOfMines)) {
+                    bombs.add(new Cell(9, j + 1, i + 1, weight));
+                    cells.add(bombs.get(bombs.size() - 1));
                 } else
-                    cells.add(new Cell(0, j, i, weight));
+                    cells.add(new Cell(0, j + 1, i + 1, weight));
             }
 
 
@@ -55,13 +53,12 @@ public class LevelsGenerator {
         root = new Pane();
         rootGameOver = new Pane(new Label("GAME OVER"));
         rootWin = new Pane(new Label("WIN"));
-        for (Cell cell : cells) {
-            if (cell.getConditon() != 9)
-                cell.getMyContent().setText("" + searchMine(cell, cells));
 
+        searchMine(numbersOfMines);
 
+        for (Cell cell : cells)
             root.getChildren().addAll(cell, cell.getMyContent());
-        }
+
 
         rootGameOver.setVisible(false);
         rootWin.setVisible(false);
@@ -80,37 +77,43 @@ public class LevelsGenerator {
 
     }
 
-    private int searchMine(Cell cell, ArrayList<Cell> cells) {
-        int digit = 0;
-        if (cell.getNumberInArray() < 80 && cell.getX() != weight)
-            if (cells.get(cell.getNumberInArray() + 1).getConditon() == 9)
-                digit++;
-        if (cell.getNumberInArray() != 0 && cell.getX() != 1)
-            if (cells.get(cell.getNumberInArray() - 1).getConditon() == 9)
-                digit++;
-        if (cell.getY() != 1) {
-            if (cells.get(cell.getNumberInArray() - weight).getConditon() == 9)
-                digit++;
-            if (cell.getX() != weight)
-                if (cells.get(cell.getNumberInArray() - weight + 1).getConditon() == 9)
-                    digit++;
-            if (cell.getNumberInArray() > 9 && cell.getX() != 1)
-                if (cells.get(cell.getNumberInArray() - weight - 1).getConditon() == 9)
-                    digit++;
-        }
-        if (cell.getY() != hight) {
-            if (cells.get(cell.getNumberInArray() + weight).getConditon() == 9)
-                digit++;
-            if (cell.getNumberInArray() < 71 && cell.getX() != weight)
-                if (cells.get(cell.getNumberInArray() + weight + 1).getConditon() == 9)
-                    digit++;
-            if (cell.getX() != 1)
-                if (cells.get(cell.getNumberInArray() + weight - 1).getConditon() == 9)
-                    digit++;
+    private void searchMine(int[] numbersOfMines) {
+
+        for (int i = 0; i < numbersOfMines.length; i++) {
+
+            if ((numbersOfMines[i]) % weight != weight - 1)
+                cells.get(numbersOfMines[i] + 1).addCondition();
+
+            if (numbersOfMines[i] % weight != 0)
+                cells.get(numbersOfMines[i] - 1).addCondition();
+
+            if (numbersOfMines[i] / weight != 0) {
+
+                cells.get(numbersOfMines[i] - weight).addCondition();
+
+                if (numbersOfMines[i] % weight != weight-1)
+                    cells.get(numbersOfMines[i] - weight + 1).addCondition();
+
+                if (numbersOfMines[i] % weight != 0)
+                    cells.get(numbersOfMines[i] - weight - 1).addCondition();
+
+            }
+            if (numbersOfMines[i] / weight != hight - 1) {
+
+                cells.get(numbersOfMines[i] + weight).addCondition();
+
+                if (numbersOfMines[i] % weight != weight-1)
+                    cells.get(numbersOfMines[i] + weight+1).addCondition();
+
+                if (numbersOfMines[i] % weight != 0)
+                    cells.get(numbersOfMines[i] + weight - 1).addCondition();
+            }
         }
 
+        for (Cell cell : cells)
+            cell.getMyContent().setText("" + cell.getConditon());
 
-        return digit;
+
     }
 
     static void gameOver() {
