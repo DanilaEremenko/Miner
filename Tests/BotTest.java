504442d96335be29307e6c2632c1993ae8ce7d30
danil <danilaeremenko@mail.ru>
2017-11-09 19:58:41 +0300

    Пытаемся писать тесты

--- Tests/BotTest.java
+++ Tests/BotTest.java
@@ -1,12 +1,23 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BotTest {
 
+    //Жалуется на создание экземпляров Cell
+    Logic logic;
+    @Before
+    public void begining(){
+        int mines[]={1,2,3,4,5,6,7};
+        logic=new Logic(9,9,mines);
+
+    }
+
+
     @Test
     public void helpMeBot(){
-
+        assertEquals(9,logic.getLevelHight());
 
 
     }
