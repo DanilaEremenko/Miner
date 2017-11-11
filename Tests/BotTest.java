1166076d47efdeae3d8f0472d95bca076ebc105a
danil <danilaeremenko@mail.ru>
2017-11-11 19:51:18 +0300

    Добавлено 3 случая, для которых можно будет сделать тесты

--- Tests/BotTest.java
+++ Tests/BotTest.java
@@ -7,17 +7,32 @@ public class BotTest {
 
     //Жалуется на создание экземпляров Cell
     Logic logic;
+
     @Before
-    public void begining(){
-        int mines[]={1,2,3,4,5,6,7};
-        logic=new Logic(9,9,mines);
+    public void begining() {
+        //для теста
+        //1
+        // int[]numbersOfMines={0,2,3,5};
+        //logic =new Logic(3,4,numbersOfMines);
+        //botCells.add(logic.getCells()[4].checkBot());
+        //botCells.add(logic.getCells()[7].checkBot());
+        //2
+        //int[]numbersOfMines={17,19,20,23,13};
+        //logic =new Logic(9,9,numbersOfMines);
+        //botCells.add(logic.getCells()[0].checkBot());
+        //3
+        //int[] mines = {7, 25, 27, 33, 55};
+        //logic = new Logic(9, 9, mines);
+        //botCells.add(logic.getCells()[0].checkBot());
+        int mines[] = {1, 2, 3, 4, 5, 6, 7};
+        logic = new Logic(9, 9, mines);
 
     }
 
 
     @Test
-    public void helpMeBot(){
-        assertEquals(9,logic.getLevelHight());
+    public void helpMeBot() {
+        assertEquals(9, logic.getLevelHight());
 
 
     }
