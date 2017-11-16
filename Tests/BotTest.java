22b791dfc45ea30f5968117aa2550e4c70ef69eb
danil <danilaeremenko@mail.ru>
2017-11-16 22:46:54 +0300

    botWinRate() easyStep()

--- Tests/BotTest.java
+++ Tests/BotTest.java
@@ -5,39 +5,36 @@ import static org.junit.Assert.*;
 
 public class BotTest {
 
+
     Logic logic;
     Bot bot;
 
-    @Before
-    public void begining() {
-        //1
-        int[] numbersOfMines = {0, 2, 3, 5};
-        logic = new Logic(3, 4, numbersOfMines);
-        bot = new Bot(logic);
-        //bot.check(4);
-        //bot.check(7);
-        //2
-        //int[]numbersOfMines={17,19,20,23,13};
-        //logic =new Logic(9,9,numbersOfMines);
-        //Bot bot=new Bot(logic);
-        //bot.check(0);
-        //3
-        //int[] mines = {7, 25, 27, 33, 55};
-        //logic = new Logic(9, 9, mines);
-        //Bot bot=new Bot(logic);
-        //bot.check(0);
-        //4
-        //int mines[] = {1, 2, 3, 4, 5, 6, 7};
-        //logic = new Logic(9, 9, mines);
-        //Bot bot=new Bot(logic);
-    }
-
 
     @Test
-    public void helpMeBot() {
+    public void botWinRate() {
+        int numberOfGames = 10000;
+        logic = new Logic(9, 9, 10);
+        bot = new Bot(logic);
+        for (int i = 0; i < numberOfGames; ) {
+            bot.helpMeBot();
+            if (bot.isGameOver()) {
+                logic.reload();
+                bot.reload();
+                i++;
+            }
+        }
+        assertEquals(true, bot.getWin() + bot.getLose() == numberOfGames);
+        assertEquals(true, (double) bot.getWin() / numberOfGames * 100 > 65);
+        System.out.println((double) bot.getWin() / numberOfGames * 100);
+
+
+    }
+
+    @Test
+    public void easyStep() {
         int massMine[] = {3, 8, 9, 11, 13, 15};
-        logic = new Logic(9, 2, massMine);
-        bot= new Bot(logic);
+        logic = new Logic(8, 2, massMine);
+        bot = new Bot(logic);
 
         bot.check(0);
         bot.check(1);
@@ -46,33 +43,38 @@ public class BotTest {
         bot.check(5);
         bot.check(6);
         bot.check(7);
-        bot.setFlagToLogicMines(3);
-        bot.setFlagToLogicMines(8);
-    }
-
-    @Test
-    public void check() {
-        bot.check(1);
-        assertEquals(true, bot.getBotCells().size() == 1);
-        bot.check(4);
-        assertEquals(true, bot.getBotCells().size() == 2);
-        bot.check(6);
-        assertEquals(true, bot.getBotCells().size() == 3);
-        bot.check(7);
-        assertEquals(true, bot.getBotCells().size() == 4);
-        bot.check(8);
-        assertEquals(true, bot.getBotCells().size() == 5);
-        bot.check(9);
-        assertEquals(true, bot.getBotCells().size() == 6);
         bot.check(10);
-        assertEquals(true, bot.getBotCells().size() == 7);
-        bot.check(11);
-        assertEquals(true, bot.getBotCells().size() == 8);
-
+        bot.check(12);
+        bot.check(14);
         bot.helpMeBot();
-        assertEquals(4, bot.getFindedMines());
-
+        assertEquals(6, bot.getFindedMines());
+        assertEquals(10, bot.getBotCells().size() );
+        assertEquals(true, bot.isGameOver());
     }
-
 
 }
+
+
+
+
+//Тестовые ситуации
+//1
+//        int[] numbersOfMines = {0, 2, 3, 5};
+//       logic = new Logic(3, 4, numbersOfMines);
+//     bot = new Bot(logic);
+//bot.check(4);
+//bot.check(7);
+//2
+//int[]numbersOfMines={17,19,20,23,13};
+//logic =new Logic(9,9,numbersOfMines);
+//Bot bot=new Bot(logic);
+//bot.check(0);
+//3
+//int[] mines = {7, 25, 27, 33, 55};
+//logic = new Logic(9, 9, mines);
+//Bot bot=new Bot(logic);
+//bot.check(0);
+//4
+//int mines[] = {1, 2, 3, 4, 5, 6, 7};
+//logic = new Logic(9, 9, mines);
+//Bot bot=new Bot(logic);
